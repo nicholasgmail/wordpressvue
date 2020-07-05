8fa2ee290f85c3b332a6a4fb71fa486447e517f1
Nikolay Us <nicholas.us.03@gmail.com>
2020-07-05 18:39:58 +0300

    ++

--- src/settings.js
+++ src/settings.js
@@ -3,6 +3,8 @@ export default {
   URL: "https://rubizhnesocks.pl.ua/",
   KEY: "ck_b2f01a2d088f9f867b5518feab3f8c0ca259d772",
   SECRET: "cs_1eb8c066cb1a9ff660fb32948ec16897e8f35b3f",
+  KEY_ORDERS: "ck_2645f3e020f700e9736fb11589bacb8e23005492",
+  SECRET_ORDERS: "cs_9a0f89bc2c5cbea1c2f089aec78f4b04549ababf",
   VERSION_3: "wc/v3",
   VERSION_2: "wc/v2",
   AXIOS: {
