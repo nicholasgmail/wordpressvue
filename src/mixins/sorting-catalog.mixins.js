3da78d076abb1e90ec6d5bc36429ce6d132106b3
Mark-1985 <alushyn5@gmail.com>
2020-07-14 10:54:06 +0300

    + миксины сортировки товаров

---
+++ src/mixins/sorting-catalog.mixins.js
@@ -0,0 +1,39 @@
+
+export default {
+  data() {
+    return {
+      sortingCatalog: { orderby: null, order: null },
+      sortingOptions: [
+        { value: { orderby: null, order: null }, text: "Исходная сортировка" },
+        { value: { orderby: "date", order: "desc" }, text: "По популярности" },
+        { value: { orderby: "date", order: "asc" }, text: "По более позднему" },
+        { value: { orderby: "price", order: "asc" }, text: "Цена по возростанию" },
+        { value: { orderby: "price", order: "desc" }, text: "Цена по убыванию" }
+      ],
+
+    }
+  },
+  async mounted() {
+    if (this.CATEGORY_ID === 36) {
+      this.sortingCategories = this.CATEGORY_ID;
+    }
+    if (this.CATEGORY_ID === 33) {
+      this.sortingCategories = this.CATEGORY_ID;
+    }
+    if (this.CATEGORY_ID === 39) {
+      this.sortingCategories = this.CATEGORY_ID;
+    }
+  },
+  watch: {
+    // отслеживание сортировок товаров по дате, популярности и стоимости
+    sortingCatalog: function() {
+      if( this.$route.path === "/shop" || this.$route.path === "/shop/" ) {
+        this.$router.push({path: "/shop"});
+      }
+      this.$route.query.page = 1;
+      this.currentPage = 1;
+      this.GET_SORTING_OPTIONS_TO_VUEX(this.sortingCatalog);
+      this.GET_PRODUCTS_FROM_API(this.$route.query.page);
+    },
+  }
+};
